d955100988e1c99cb6c927f9015c4efdd2052138
skyeshere <skyepluh@gmail.com>
2025-11-15 01:33:29 +0000

    QOL

--- src/App.java
+++ src/App.java
@@ -27,21 +27,43 @@ public class App
 
     public ArrayList<String> userInput()
     {
-        System.out.println("Welcome to the spit merger! Please note: You may have to format your splits after merging depending on your needs.");
-        System.out.println("Please enter the names of the split files you want to merge (including the `.lss` file extension). Enter a non-lss file string to continue to the merge");
+        if((int)(Math.random()*10) % 2 == 0) //Shoutout to MarvJungs for thinking of this idea!!!!! PLEASE DONATE IM BEGGIUNG
+        {
+            System.out.println("Consider donating! You can do that on https://streamelements.com/skyeves/tip");
+        }
+
+        System.out.println("Welcome to the spit merger! Please note: You may have to format your splits after merging depending on your needs. \n");
+        System.out.println("Please enter the names of the split files you want to merge (including the `.lss` file extension). Press 'Enter' with no input to continue.");
 
         String result = "";
-        
+        int counter = 1;
         do
         {
+            System.out.print("item " + counter + ": ");
             result = input.nextLine();
 
-            if(result.toLowerCase().contains(".lss"))
+            if(result.length() >= 5) //if the resulting string is at a length to be a possible split file
             {
-                args.add(result);
+                if(result.substring(result.length() - 4).toLowerCase().equals(".lss")) //check for file extension
+                {
+                    args.add(result);
+                }
             }
+            else //this is not a great solution to be honest
+            {
+                break;
+            }
+
+            counter++;
         }
-        while(result.toLowerCase().contains(".lss"));
+        while(result.substring(result.length() - 4).toLowerCase().equals(".lss"));
+
+        if(args.size() <= 1)
+        {
+            System.out.println("Please enter at least 2 split files to merge.");
+            System.out.println("You entered: " + args.size() + " splits");
+            System.exit(1);
+        }
 
         return args;
     }
